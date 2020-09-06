4650a24ffdde7e3cc86031660ebdc219707bb11f
Meng Lan <menglan36@gmail.com>
2020-09-06 10:25:12 -0400

    Add files via upload

---
+++ google api.R
@@ -0,0 +1,29 @@
+
+library(RCurl)
+library(RJSONIO)
+
+construct.geocode.url <- function(address, return.call = "json", sensor = "false") {
+  root <- "http://maps.google.com/maps/api/geocode/"
+  u <- paste(root, return.call, "?address=", address, "&sensor=", sensor, sep = "")
+  return(URLencode(u))
+}
+
+gGeoCode <- function(address,verbose=FALSE) {
+  if(verbose) cat(address,"\n")
+  u <- construct.geocode.url(address)
+  doc <- getURL(u)
+  x <- fromJSON(doc,simplify = FALSE)
+  if(x$status=="OK") {
+    lat <- x$results[[1]]$geometry$location$lat
+    lng <- x$results[[1]]$geometry$location$lng
+    return(c(lat, lng))
+  } else {
+    return(c(NA,NA))
+  }
+}
+
+
+x <- gGeoCode("20 River Road, NY")
+x
+
+
